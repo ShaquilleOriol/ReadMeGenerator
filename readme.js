cb7fda081ff3534a6e3a3741c3220702bd85e24f
Shaquille Oriol <shaquille.oriol@gmail.com>
2021-03-15 18:13:07 -0500

    syntax Update

--- readme.js
+++ readme.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-const writefileAsync = util.promisify(fs.writeFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 
 const promptUser = () => {
